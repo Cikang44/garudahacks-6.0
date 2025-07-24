95c176a04896a488d59c9778f9f770cebb84c0f1
snailsquid <hexagononyt@gmail.com>
2025-07-24 20:52:25 +0700

    feat: implemented color selection

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,16 +1,13 @@
+"use client";
 import ColorElement from "@/components/editor/color";
 import ApparelPreview from "@/components/editor/preview";
 import { colorId } from "@/components/editor/color";
+import { useState } from "react";
 const apparelId = 0;
 
 type cell = {
   selectedPatternId: number;
   selectedColorId: number;
-};
-
-const selectedCell: cell = {
-  selectedColorId: 2,
-  selectedPatternId: 3,
 };
 
 type patternId = number;
@@ -19,28 +16,58 @@ type patternDictionary = { [key: patternId]: patternFile };
 type colorTable = colorId[];
 type hexColor = string;
 type colorDictionary = { [key: colorId]: hexColor };
-const allPatterns: patternDictionary = {
+export const allPatterns: patternDictionary = {
   // ID : FileName
   198474: "Batik.png",
 };
-const allColors: colorDictionary = {
+export const allColors: colorDictionary = {
   // ID : hex
-  234567: "123f4",
+  0: "0c0c0d",
+  1: "0386cc",
+  2: "345a1e",
 };
 
-const apparelColors: colorTable = [];
+const apparelColors: colorTable = [0, 1, 2];
 
 export default function Editor() {
+  const [selectedCell, setSelectedCell] = useState<cell>({
+    selectedColorId: 0,
+    selectedPatternId: 0,
+  });
+
+  function selectColor(colorSelect: colorId) {
+    if (!selectedCell) return;
+    setSelectedCell((prev) => {
+      if (!prev) return prev;
+      else {
+        console.log(colorSelect);
+        return { ...prev, selectedColorId: colorSelect };
+      }
+    });
+  }
+
+  function selectPattern() {}
   return (
-    <div>
-      <div id="apparel-editor">
+    <div className="w-screen h-screen flex flex-row justify-center items-center py-10 gap-10">
+      <div
+        id="apparel-editor"
+        className="w-[70%] items-center justify-center flex flex-row bg-black h-full"
+      >
         <div id="pattern-palette"></div>
         <div id="apparel-preview">
           <ApparelPreview></ApparelPreview>
         </div>
-        <div id="color-palette"></div>
+        <div id="color-palette" className="flex flex-row gap-5">
+          {apparelColors.map((color) => (
+            <ColorElement
+              onColorSelect={selectColor}
+              key={color}
+              previewColor={color}
+            ></ColorElement>
+          ))}
+        </div>
       </div>
-      <div id="info">
+      <div id="info" className="w-[20%] bg-black h-full">
         <h2 id="pattern-name"></h2>
         <div id="pattern-preview"></div>
         <div id="pattern-desc"></div>
--- components/editor/color.tsx
+++ components/editor/color.tsx
@@ -1,4 +1,26 @@
+"use client";
+import { allColors } from "@/app/editor/page";
 export type colorId = number;
-export default function ColorElement(previewColor: colorId) {
-  return <div></div>;
+const size = "80px";
+
+export default function ColorElement({
+  previewColor,
+  onColorSelect,
+}: {
+  previewColor: colorId;
+  onColorSelect: (colorId: colorId) => void;
+}) {
+  return (
+    <button
+      onClick={() => {
+        onColorSelect(previewColor);
+      }}
+      className={` cursor-pointer rounded-lg`}
+      style={{
+        backgroundColor: `#${allColors[previewColor]}`,
+        width: size,
+        height: size,
+      }}
+    ></button>
+  );
 }
